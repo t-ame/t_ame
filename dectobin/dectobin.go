fc0ce7c75bbc5ec24a08386d2932c986ccda0ca0
Toya Amechi <toya.amechi@gmail.com>
2020-03-26 18:11:52 +0100

    decimal to binary conversin

--- dectobin/dectobin.go
+++ dectobin/dectobin.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-func readNConvert() {
-
+func readNConvert() (string, int) {
+	fmt.Println("Please enter a number to be converted to binary.")
+	var decNum int
+	var binNum string
+	s, err := fmt.Scan(&decNum)
+	if err == nil {
+		fmt.Printf("You entered: %d\n", decNum)
+		binNum = strconv.FormatInt(int64(decNum), 2)
+	}
+	return binNum, s
 }
 
 func main() {
-
-	fmt.Println("Please enter number to be converted to binary.")
-
-	var binNum int
-
-	s, err := fmt.Scan(&binNum)
-
-	fmt.Printf("You entered %d\n", s)
-	if err != nil {
-		fmt.Println(err.Error())
+	binNum := ""
+	for {
+		binary, count := readNConvert()
+		if count != 0 {
+			binNum = binary
+			break
+		} else {
+			fmt.Println("A number is expected.")
+		}
 	}
-
+	fmt.Printf("Binary equivalent: %s\n", binNum)
 }
