d4039c56eee8a6a0a6aadfbf0bdabedb8998a49c
Toya Amechi <toya.amechi@longbridgetech.com>
2020-03-29 13:38:54 +0100

    task 1

--- first-app/app.js
+++ first-app/app.js
@@ -1,24 +1,24 @@
-// const Logger = require('./logger');
-// const logger = new Logger();
-
-// logger.on('logging', (arg) => {
-//     console.log('Logging:', arg.message);
-// })
-
-// logger.log('Some healthy message!!!');
-
 const http = require('http');
 const fs = require('fs');
+const qs = require('querystring');
 
 const server = http.createServer((req, res) => {
-    if(req.method === 'POST'){
-        if(req.url === '/'){
-            res.write(`<b>Goodbye world!!!</b>`);
-            res.end();
-        }        
+    if(req.method === 'POST'){      
         if(req.url === '/message'){
-            res.write(JSON.stringify(['My', 'Home', 'Is', 'Not Here']));
-            res.end();
+            var body='';
+            req.on('data', function (data) {
+                body +=data;
+            });
+            req.on('end',function(){
+                var POST =  JSON.parse(JSON.stringify(qs.parse(body)));
+                console.log(POST);
+                fs.appendFile('message.txt', `${POST.message}\n`, function (err) {
+                    if (err) throw err;
+                    console.log('Saved!');
+                  });
+                var myReadStream = fs.createReadStream(__dirname + '/message.html', 'utf8');
+                myReadStream.pipe(res);
+            });
         }
     } else if(req.method === 'GET'){
         if(req.url === '/'){
@@ -27,11 +27,7 @@ const server = http.createServer((req, res) => {
             myReadStream.pipe(res);
         }   
     }
-});
-
-
-server.listen(3000);
-
-console.log('listening on port 3000...');
+}).listen(8080);
+console.log('listening on port 8080...');
 
 
